6f7b2915525cf4a537bbbb20b230b324ba7cbdb6
felzer121 <37536146+felzer121@users.noreply.github.com>
2020-12-28 14:12:55 +0300

    services

---
+++ src/app/services/some.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class SomeDataService {
+  header: number = 1;
+  services: number = 1;
+  works: number = 1;
+  reviews: number = 1;
+}
+export class SomeService {
+
+  constructor() { }
+}
+
