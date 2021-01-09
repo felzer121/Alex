bd43e39230b9571b1a2180b4b547a063be8e0200
Damir Mansurov <rave9373@gmail.com>
2021-01-09 04:00:10 +0300

    send to mailbox, need to complete

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,6 +1,5 @@
-import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import {SomeDataService} from "../../services/some.service";
-import { ViewportScroller } from '@angular/common';
+import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import {ContactUsModel} from '../contact/contactUsModel';
 
 @Component({
   selector: 'app-contact',
@@ -8,9 +7,14 @@ import { ViewportScroller } from '@angular/common';
   styleUrls: ['./contact.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
+
 export class ContactComponent implements OnInit {
   constructor() {
   }
   ngOnInit(): void {
+  }
+  sendMessage(): void {
+  //sendMessage(contactUs: ContactUsModel): void {
+    console.log('Отправка на бэк');
   }
 }
